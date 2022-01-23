98c1961a15f0ecf457585a53508041a1e4183bef
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-01-23 15:16:08 +0300

    add task 3 examples

--- src/main/java/com/tms/Main.java
+++ src/main/java/com/tms/Main.java
@@ -5,8 +5,19 @@ import com.tms.task3.Task3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("==================================");
-        System.out.println("Is month has valid number of days?");
-        System.out.println("The result is " + Task3.validateMonth(45));
+//        Task 3.1 example start
+//        System.out.println("Is month has valid number of days?");
+//        System.out.println("The result is " + Task3.validateMonth(45));
+//        Task 3.1 example end
+
+//        Task 3.2 example start
+//        System.out.println("Сегодня второй день недели, а значит это  " + Task3.nameOfDay(20));
+//        Task 3.2 example end
+
+//        Task 3.3 example start
+//        System.out.println(Task3.checkNumberConditions(7));
+//        Task 3.3 example end
         System.out.println("==================================");
+
     }
 }
--- src/main/java/com/tms/task3/Task3.java
+++ src/main/java/com/tms/task3/Task3.java
@@ -10,4 +10,55 @@ public class Task3 {
             return Boolean.FALSE;
         }
     }
+
+    public static String checkNumberConditions(double number) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (number > 0) {
+            stringBuilder.append("Число положительное");
+            if (number % 1 == 0) {
+                stringBuilder.append(", целое");
+                if (number % 2 == 0) {
+                    stringBuilder.append(", четное");
+                } else {
+                    stringBuilder.append(", нечетное");
+                }
+            } else {
+                stringBuilder.append(", дробное");
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    public static String nameOfDay(int number) {
+        if (number > 7 || number < 1)
+            throw new IllegalArgumentException("Номер дня недели не может быть больше 7 и меньше 1");
+
+        String result = null;
+        switch (number) {
+            case 1:
+                result = "Понедельник";
+                break;
+            case 2:
+                result = "Вторник";
+                break;
+            case 3:
+                result = "Среда";
+                break;
+            case 4:
+                result = "Четверг";
+                break;
+            case 5:
+                result = "Пятница";
+                break;
+            case 6:
+                result = "Суббота";
+                break;
+            case 7:
+                result = "Воскресенье";
+                break;
+            default:
+                break;
+        }
+        return result;
+    }
 }
