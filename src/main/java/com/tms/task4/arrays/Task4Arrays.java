eeffeda969aac18fdc5894c265b15f5a0a470636
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-01-23 17:55:25 +0300

    add task 4 examples

---
+++ src/main/java/com/tms/task4/arrays/Task4Arrays.java
@@ -0,0 +1,13 @@
+package com.tms.task4.arrays;
+
+public class Task4Arrays {
+    public static void main(String[] args) {
+        System.out.println("Work with arrays");
+
+        int[] array = new int[5];
+        int[] array1 = new int[] {1,2,3,4,5};
+        long[] longs = new long[] {1, 123553, 42134123};
+        String[] strings = new String[] {"24", "asdfasdf", "13442", "asfr"};
+        Pair[] pairs = new Pair[] {new Pair(1, "1"), new Pair(2, "2")};
+    }
+}
