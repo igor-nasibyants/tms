6fef74363f9912971bb7052373bd0ce5ec039282
HaNiks <haniks2899@gmail.com>
2022-02-01 23:18:04 +0300

    Task2 is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task2.java
@@ -0,0 +1,17 @@
+package com.tms.homework.han.task1;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[] arr = new int[50];
+        System.out.println("Вывод с 1 до 99:");
+        for (int i = 0, j = 1; i < arr.length; i++, j += 2) {
+            arr[i] = j;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Вывод с 99 до 0:");
+        for (int i = arr.length - 1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
