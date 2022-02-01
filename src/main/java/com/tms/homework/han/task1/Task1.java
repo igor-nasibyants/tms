15c29546351ff70e9d7dfd45b5387bb7ea308ea0
HaNiks <haniks2899@gmail.com>
2022-02-01 23:16:27 +0300

    Task1 is ready

--- src/main/java/com/tms/homework/han/task1/Task1.java
+++ src/main/java/com/tms/homework/han/task1/Task1.java
@@ -2,6 +2,15 @@ package com.tms.homework.han.task1;
 
 class Task1 {
     public static void main(String[] args) {
-
+        int[] arr = new int[10];
+        System.out.println("Вывод в столбик");
+        for (int i = 0, j = 2; i < arr.length; i++, j += 2) {
+            arr[i] = j;
+            System.out.println(arr[i]);
+        }
+        System.out.println("Вывод в строку");
+        for (int key : arr) {
+            System.out.print(key + " ");
+        }
     }
 }
