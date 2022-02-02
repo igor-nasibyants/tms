f659aa19d60c2d66d7a970bd82a9b0391f086f18
Nikita <nikita1998-15@mail.ru>
2022-02-02 17:12:44 +0300

    add method TaskOne in TwoDimArray class

--- src/main/java/com/tms/homework/astakhnovich/Main.java
+++ src/main/java/com/tms/homework/astakhnovich/Main.java
@@ -1,12 +1,15 @@
 package com.tms.homework.astakhnovich;
 
 
+import com.tms.homework.astakhnovich.task1.Task1;
 import com.tms.homework.astakhnovich.task3.OneDimArray;
+import com.tms.homework.astakhnovich.task3.TwoDimArray;
 
 public class Main {
     public static void main(String[] args) {
        task1();
        oneDimArray();
+       twoDimArray();
     }
 
     public static void task1() {
@@ -14,11 +17,12 @@ public class Main {
     }
 
     public static void oneDimArray() {
-        OneDimArray.taskOne();
-        OneDimArray.taskTwo();
-        OneDimArray.taskThree();
+//        OneDimArray.taskOne();
+//        OneDimArray.taskTwo();
+//        OneDimArray.taskThree();
     }
 
     public static void twoDimArray() {
+        TwoDimArray.TaskOne();
     }
 }
