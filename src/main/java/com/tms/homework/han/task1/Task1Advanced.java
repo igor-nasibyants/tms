c2a7e097a88b2805402ec11403244f11e469af75
Mikita <haniks2899@gmail.com>
2022-02-02 15:16:51 +0300

    Task1Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task1Advanced.java
@@ -0,0 +1,17 @@
+package com.tms.homework.han.task1;
+
+public class Task1Advanced {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int mul = 1;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            if (arr[i] % 3 == 0 && arr[i] != 0) {
+                mul *= i;
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Произведение чисел, кратных 3 в отрезке [0;" + arr.length + "]: " + mul);
+    }
+}
