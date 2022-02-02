48376b1a2938179247164a554a377f59b0ee3a7d
Mikita <haniks2899@gmail.com>
2022-02-02 15:54:10 +0300

    Task4Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task4Advanced.java
@@ -0,0 +1,20 @@
+package com.tms.homework.han.task1;
+
+public class Task4Advanced {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        System.out.print("Массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ((int) (Math.random() * 15));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        int min = arr[0];
+        for (int key : arr) {
+            if (min >= key) {
+                min = key;
+            }
+        }
+        System.out.println("Минимальный элемент в массиве: " + min);
+    }
+}
