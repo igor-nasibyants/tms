3338caea0555dc8cd49e537977fa93af0436b405
Nikita <nikita1998-15@mail.ru>
2022-02-02 17:12:44 +0300

    add method TaskOne in TwoDimArray class

--- src/main/java/com/tms/homework/astakhnovich/task3/TwoDimArray.java
+++ src/main/java/com/tms/homework/astakhnovich/task3/TwoDimArray.java
@@ -1,6 +1,18 @@
 package com.tms.homework.astakhnovich.task3;
 
+import java.util.Scanner;
+
 public class TwoDimArray {
 
+    public static void TaskOne(){
+        Scanner scan = new Scanner(System.in);
+        int [][] twoDimArray = new int [scan.nextInt()][scan.nextInt()];
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                System.out.print(" " + twoDimArray[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
