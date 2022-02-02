53492891b3fe773c20f1a61cc12c68d461de857f
Mikita <haniks2899@gmail.com>
2022-02-02 15:28:36 +0300

    Task2Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task2Advanced.java
@@ -0,0 +1,20 @@
+package com.tms.homework.han.task1;
+
+public class Task2Advanced {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int sum = 0;
+        int count = 0;
+        System.out.print("Массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+            if (i % 2 != 0) {
+                sum += arr[i];
+                count++;
+            }
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Среднее арифмитическре элементов с нечетными номерами: " + sum / count);
+    }
+}
