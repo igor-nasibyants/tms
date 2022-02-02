5443d3d1bce1af26a22c6cc2b634cbc90b4139d6
Mikita <haniks2899@gmail.com>
2022-02-02 16:46:29 +0300

    Task5Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task5Advanced.java
@@ -0,0 +1,31 @@
+package com.tms.homework.han.task1;
+
+import java.util.Arrays;
+
+public class Task5Advanced {
+    public static void main(String[] args) {
+        int n = 10;
+        int[] arr = new int[n];
+        System.out.print("Массив: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = ((int) (Math.random() * 15));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Массив после 'сжатия': ");
+        for (int i = 0, j = 0; i < n; i++, j += 2) {
+            arr[i] = arr[j];
+            n--;
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Массив с нулями: ");
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 != 0) {
+                arr[i] = 0;
+            }
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
+
