670ce85d406c250003b4ffbd41b20db464c704f6
Nikita <nikita1998-15@mail.ru>
2022-02-02 16:51:25 +0300

    add some tasks

---
+++ src/main/java/com/tms/homework/astakhnovich/Main.java
@@ -0,0 +1,24 @@
+package com.tms.homework.astakhnovich;
+
+
+import com.tms.homework.astakhnovich.task3.OneDimArray;
+
+public class Main {
+    public static void main(String[] args) {
+       task1();
+       oneDimArray();
+    }
+
+    public static void task1() {
+//        Task1.BinaryOperations();
+    }
+
+    public static void oneDimArray() {
+        OneDimArray.taskOne();
+        OneDimArray.taskTwo();
+        OneDimArray.taskThree();
+    }
+
+    public static void twoDimArray() {
+    }
+}
