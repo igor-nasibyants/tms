da09891d29ec38c8b26f43852b1610a5b372afc8
Matvei Shumchenia <shumchenia.mat@gmail.com>
2022-02-02 14:41:06 +0300

    add dz in Task1

--- src/main/java/com/tms/homework/shumchenia/Task1.java
+++ src/main/java/com/tms/homework/shumchenia/Task1.java
@@ -1,4 +1,25 @@
 package com.tms.homework.shumchenia;
 
-public class Task1 {
+public class Task1{
+
+    public static void main(String[] args) {
+        char[][] array =new char[8][8];
+        for(int i=0;i<array.length;i++) {
+            for(int j=0;j<array.length;j++){
+                if(i%2==0&&j%2!=0){
+                    array[i][j]='#';
+                }
+                else{
+                    array[i][j]='@';
+                }
+            }
+        }
+        for (char[] chars : array) {
+            System.out.println();
+            for (int j = 0; j < array.length; j++) {
+                System.out.print(chars[j]);
+            }
+        }
+
+    }
 }
