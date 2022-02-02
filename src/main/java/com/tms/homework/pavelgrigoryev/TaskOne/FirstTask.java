8bf7787f3e50a0d2b7cc46003ad007daeda060c2
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-02 20:08:08 +0300

    add FirstTask

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/TaskOne/FirstTask.java
@@ -0,0 +1,19 @@
+package com.tms.homework.pavelgrigoryev.TaskOne;
+
+public class FirstTask {
+    public static void main(String[] args) {
+        run();
+    }
+    public static void run(){
+        int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        System.out.println("Строка: ");
+        for (int j : array) {
+            System.out.print(j + " ");
+        }
+        System.out.println();
+        System.out.println("Столбик: ");
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+}
--- src/main/java/com/tms/homework/pavelgrigoryev/TaskOne/Test.java
+++
@@ -1,7 +0,0 @@
-package com.tms.homework.pavelgrigoryev.TaskOne;
-
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello world!!!");
-    }
-}
