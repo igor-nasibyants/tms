f21166604279866985df6279fc6cff9bea384a5a
Mikita <haniks2899@gmail.com>
2022-02-02 12:59:04 +0300

    Task8 is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task8.java
@@ -0,0 +1,37 @@
+package com.tms.homework.han.task1;
+
+public class Task8 {
+    public static void main(String[] args) {
+        int[] firstArr = new int[10];
+        int[] secondArr = new int[10];
+        double[] thirdArr = new double[10];
+        int count = 0;
+        System.out.print("Первый массив: ");
+        printArr(firstArr);
+        System.out.print("Второй массив: ");
+        printArr(secondArr);
+        System.out.print("Третий массив: ");
+        for (int i = 0; i < thirdArr.length; i++) {
+            if (firstArr[i] == 0 && secondArr[i] == 0) {
+                thirdArr[i] = 0;
+            }
+            else {
+                thirdArr[i] = (double)firstArr[i] / secondArr[i];
+            }
+            System.out.print(thirdArr[i] + " ");
+            if (thirdArr[i] % 1 == 0) {
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println("Количество целых элементов в 3 массиве: " + count);
+    }
+
+    public static void printArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ((int) (Math.random() * 9));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+}
