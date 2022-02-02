5d2ab6bb89fd50d106666a43bb65942c01ec0b02
Никита <nikita1998-15@mail.ru>
2022-02-02 20:05:35 +0300

    add aettingNumber, arrayFilling, printTwoDimArray, sumEvenElementsMainDiagonal, elementsBelowMainDiagonal clases

--- src/main/java/com/tms/homework/astakhnovich/task3/TwoDimArray.java
+++ src/main/java/com/tms/homework/astakhnovich/task3/TwoDimArray.java
@@ -1,18 +1,59 @@
 package com.tms.homework.astakhnovich.task3;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TwoDimArray {
+    Integer matrixSize;
+    Integer [][] twoDimArray;
 
-    public static void TaskOne(){
+    public void gettingNumber(){
         Scanner scan = new Scanner(System.in);
-        int [][] twoDimArray = new int [scan.nextInt()][scan.nextInt()];
+        do {
+            System.out.println("Please enter a positive number!");
+            while (!scan.hasNextInt()) {
+                System.out.println("That not a number!");
+                scan.next();
+            }
+            matrixSize = scan.nextInt();
+        } while (matrixSize <= 0);
+        twoDimArray = new Integer[matrixSize][matrixSize];
+    }
+
+    public void arrayFilling(){
+        Random randomNumber = new Random();
         for (int i = 0; i < twoDimArray.length; i++) {
             for (int j = 0; j < twoDimArray[i].length; j++) {
-                System.out.print(" " + twoDimArray[i][j] + " ");
+                twoDimArray [i][j] = randomNumber.nextInt(50);
+            }
+        }
+    }
+
+    public void printTwoDimArray() {
+        for (int i = 0; i < twoDimArray.length; i++) {
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+                System.out.print(twoDimArray[i][j] + " ");
             }
             System.out.println();
         }
     }
 
+    public void sumEvenElementsMainDiagonal(){
+        Integer sum = 0;
+        for (int i = 0; i < twoDimArray.length; i++) {
+            sum += twoDimArray[i][i];
+        }
+        System.out.println("the sum of the even elements of the main diagonal = " + sum);
+    }
+
+    public void elementsBelowMainDiagonal(){
+        Integer sum = 0;
+        for (int i = 1; i < twoDimArray.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if(twoDimArray[i][j] % 2 != 0){
+                    System.out.println(twoDimArray[i][j]);
+                }
+            }
+        }
+    }
 }
