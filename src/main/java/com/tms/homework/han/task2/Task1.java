09b4381cf03c909e278391e4f04a7dfea73c56b8
HaNiks <haniks2899@gmail.com>
2022-02-03 23:31:39 +0300

    Refactor and fix Task1

--- src/main/java/com/tms/homework/han/task2/Task1.java
+++ src/main/java/com/tms/homework/han/task2/Task1.java
@@ -5,12 +5,13 @@ public class Task1 {
         int[][] arr = Main.getArr();
         int sum = 0;
         for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i] == arr[j]) {
-                    sum +=arr[i][j];
+            for (int j = 0; j < i + 1; j++) {
+                if (arr[i] == arr[j] && arr[i][j] % 2 == 0) {
+                    sum += arr[i][j];
+                    break;
                 }
             }
         }
-        System.out.println("Сумма массива: " + sum);
+        System.out.println("Сумма четных элементов на главной диагонали: " + sum);
     }
 }
