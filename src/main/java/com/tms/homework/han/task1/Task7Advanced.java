6b6244e7c8f6684f9f6fa5a9ebbd148f8b2b6500
HaNiks <haniks2899@gmail.com>
2022-02-03 20:51:57 +0300

    Task7Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task7Advanced.java
@@ -0,0 +1,23 @@
+package com.tms.homework.han.task1;
+
+//    Подсчитать, сколько раз встречается элемент с заданным значением
+
+public class Task7Advanced {
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        int checkNumber = 2;
+        int count = 0;
+        System.out.print("Полученный массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ((int) (Math.random() * 4));
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        for (int key : arr) {
+            if (key == checkNumber) {
+                count++;
+            }
+        }
+        System.out.println("Значение " + checkNumber + " встречается " + count + " раз");
+    }
+}
