22bce745338fb70482f58be3c01ab12d287237ec
HaNiks <haniks2899@gmail.com>
2022-02-03 21:45:26 +0300

    Task9Advanced is ready

---
+++ src/main/java/com/tms/homework/han/task1/Task9Advanced.java
@@ -0,0 +1,20 @@
+package com.tms.homework.han.task1;
+
+//      Найти наименьший элемент среди элементов с четными индексами массива
+
+public class Task9Advanced {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        int min = Integer.MAX_VALUE;
+        System.out.print("Полученный массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = ((int) (Math.random() * 10));
+            System.out.print(arr[i] + " ");
+            if (i % 2 == 0 && min >= arr[i]) {
+                min = arr[i];
+            }
+        }
+        System.out.println();
+        System.out.println("Наименьший элемент в массиве: " + min);
+    }
+}
