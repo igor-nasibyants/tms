b77a1d1e99cd6d1857d3557c8afbbbc049c85293
pavel.shvedov <composer.shvedov@gmail.com>
2022-02-03 21:51:56 +0300

    Delete all my classes

--- src/main/java/com/tms/homework/shvedov/JavaClassNew.java
+++
@@ -1,7 +0,0 @@
-package com.tms.homework.shvedov;
-
-public class JavaClassNew { //Task 1
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
-}
--- src/main/java/com/tms/homework/shvedov/Task1.java
+++
@@ -1,5 +0,0 @@
-package com.tms.homework.shvedov;
-
-public class Task1 {
-    //Второй файл я тоже сделал
-}
