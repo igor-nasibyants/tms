33e4570b8cfa12033f601bfe771c776755d2cfbb
HaNiks <haniks2899@gmail.com>
2022-02-03 23:26:48 +0300

    Task2 is ready

---
+++ src/main/java/com/tms/homework/han/task2/Task2.java
@@ -0,0 +1,18 @@
+package com.tms.homework.han.task2;
+
+//        Вывести нечетные элементы находящиеся под главной
+//        диагональю(включительно)
+
+public class Task2 {
+    public static void main(String[] args) {
+        int[][] arr = Main.getArr();
+        System.out.println("Нечетные элементы находящиеся под главной диагональю(включительно):");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (arr[i][j] % 2 != 0) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+        }
+    }
+}
