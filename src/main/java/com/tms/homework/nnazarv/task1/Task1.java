5fde11c21aa3bde4ccb274c6479afddda1c2b3c4
NNazarV <nazarenko-nazar@bk.ru>
2022-02-03 21:22:20 +0300

    Add out in a column

--- src/main/java/com/tms/homework/nnazarv/task1/Task1.java
+++ src/main/java/com/tms/homework/nnazarv/task1/Task1.java
@@ -2,7 +2,19 @@ package com.tms.homework.nnazarv.task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        int[] array = new int[10];
+        int a = 0;
+        int i;
+        for (i = 1; i <= 20; i++) {
+            if (i % 2 == 0) {
+                array[a] = i;
+                System.out.print(array[a] + " ");
+                a++;
+            }
+        }
+        for (i = 0; i< array.length; i++ ){
+            System.out.print("\n" + array[i]);
+        }
 
     }
-
 }
