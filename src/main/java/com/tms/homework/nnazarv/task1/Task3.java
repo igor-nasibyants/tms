989da035fd5343e330e3120da1150ef12c045ec5
NNazarV <nazarenko-nazar@bk.ru>
2022-02-04 18:06:26 +0300

    Done task 3

---
+++ src/main/java/com/tms/homework/nnazarv/task1/Task3.java
@@ -0,0 +1,22 @@
+package com.tms.homework.nnazarv.task1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int[] array = new int[15];
+
+
+        for (int i = 0; i <= array.length  - 1; i++){
+            int random_number = (int) (Math.random() * 99);
+            array[i] = random_number;
+            System.out.print(array[i] + " ");
+        }
+
+        System.out.println();
+
+        for (int i = 0; i <= array.length  - 1; i++){
+            if (array[i] % 2 == 0){
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+}
