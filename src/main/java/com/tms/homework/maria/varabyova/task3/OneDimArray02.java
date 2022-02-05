2f62f7360af28eac7f42d68f047eb5c854a229aa
Maria Varabyova <m.warabjowa@gmail.com>
2022-02-05 16:23:26 +0300

    Add OneDimArray02.java in maria.varabyova.task3

---
+++ src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray02.java
@@ -0,0 +1,17 @@
+package com.tms.homework.maria.varabyova.task3;
+
+/* 2)Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в
+строку, а затем этот же массив выведите на экран тоже в строку, но в обратном
+порядке (99 97 95 93 … 7 5 3 1).
+
+public class OneDimArray02 {
+    public static void main(String args[]){
+        int n=50;
+        int arr =new arr[n];
+
+        for(int i=0; i<n, i++) {
+            int[i]=
+        }
+
+    }
+}
