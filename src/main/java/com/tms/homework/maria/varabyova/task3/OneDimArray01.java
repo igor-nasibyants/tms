eadd298fd7b35656b5882d2065e1854b7da2d30f
Maria Varabyova <m.warabjowa@gmail.com>
2022-02-05 17:34:41 +0300

    Change OneDimArray02.java in maria.varabyova.task3

--- src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray01.java
+++ src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray01.java
@@ -26,15 +26,10 @@ public class OneDimArray01 {
             }
         }
 
-
-
         for (int i = 0; i < a.length; i++) {
             System.out.print(a[i] + " ");
         }
-
         System.out.println();
-
-
 
         for (int i=0; i<a.length; i++) {
             System.out.println(a[i]);
