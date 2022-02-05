8c8757754ebba4faebefe10d89f2b35c024e4ddb
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-05 14:59:08 +0300

    added an additional task in SecondTask

--- src/main/java/com/tms/homework/pavelgrigoryev/task2/SecondTask.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task2/SecondTask.java
@@ -6,10 +6,14 @@ public class SecondTask {
     }
     public static void run(){
         int[] array = new int[99];
+        int count = 0;
+        int count2 = 0;
         System.out.println("Вывод нечётных чисел от 1 до 99 : ");
         for (int i = 0; i < array.length; i += 2) {
-           array[i] = i + 1;
+            array[i] = i + 1;
             System.out.print(array[i] + " ");
+            count += array[i];
+            count2 ++;
         }
         System.out.println();
         System.out.println("Вывод нечётных чисел в обратном порядке : ");
@@ -17,5 +21,7 @@ public class SecondTask {
             array[i] = 99 - i;
             System.out.print(array[i] + " ");
         }
+        System.out.println();
+        System.out.println("Среднее арифметическое значение элементов с нечётными номерами равно : " + count/count2);
     }
 }
