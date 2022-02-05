22b8d423a5895c8eb33e04af6a041babc30e795f
Maria Varabyova <m.warabjowa@gmail.com>
2022-02-05 15:52:54 +0300

    Add OneDimArray01.java in maria.varabyova.task3

---
+++ src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray01.java
@@ -0,0 +1,19 @@
+package com.tms.homework.maria.varabyova.task3;
+
+/* 1)Создайте массив из всех чётных чисел от 2 до 20 и выведите элементы
+массива на экран сначала в строку, отделяя один элемент от другого пробелом, а
+затем в столбик (отделяя один элемент от другого началом новой строки). Перед
+созданием массива подумайте, какого он будет размера.
+ */
+public class OneDimArray01 {
+    public static void main(String args[]) {
+        int a[] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        System.out.println("Значения массива ");
+        for(int i=0; i<a.length; i++) {
+            System.out.println(a[i] + " ");
+        }
+        for(int i=1;i<a.length; i++){
+            System.out.print(a[i]+" ");
+        }
+    }
+}
