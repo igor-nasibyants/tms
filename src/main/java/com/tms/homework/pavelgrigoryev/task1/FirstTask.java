3ae86dbc5095fb53d55731d13b62acd79275399e
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-05 14:35:20 +0300

    added an additional task in FirstTask

--- src/main/java/com/tms/homework/pavelgrigoryev/task1/FirstTask.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task1/FirstTask.java
@@ -6,14 +6,19 @@ public class FirstTask {
     }
     public static void run(){
         int[] array = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int count = 1;
         System.out.println("Строка: ");
-        for (int j : array) {
-            System.out.print(j + " ");
+        for (int k : array) {
+            System.out.print(k + " ");
+            if (k % 3 == 0) {
+                count *= k;
+            }
         }
         System.out.println();
         System.out.println("Столбик: ");
         for (int i : array) {
             System.out.println(i);
         }
+        System.out.println("Произведение элементов массива кратных 3 : " + count);
     }
 }
