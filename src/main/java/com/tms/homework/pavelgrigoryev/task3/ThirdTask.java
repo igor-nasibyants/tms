1ecdc39677a99e7c05de3eb3d09c1cb544ca5867
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-05 21:24:57 +0300

    added an additional task in ThirdTask

--- src/main/java/com/tms/homework/pavelgrigoryev/task3/ThirdTask.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task3/ThirdTask.java
@@ -7,15 +7,20 @@ public class ThirdTask {
     public static void jump() {
         int[] array ;
         int count = 0;
+        int count2 = 0;
+        int c = (int) (Math.random() * 3 + 1);
         for (int i = 0; i < 15; i++){
             array = new int[(int)(Math.random() * 99)];
             System.out.print(array.length + " ");
             if (array.length % 2 == 0) {
+                count2 += array.length;
                 count++;
             }
         }
         System.out.println();
+        System.out.println(count2);
         System.out.println("Количество четных элементов : " + count);
+        System.out.println("Средне арифметическое четных элементов массива " + (double) count2/count + ", превосходящее число С равное " + c);
     }
 }
 
