0cfad160e971ee9dbf06278278df140399f7bf3a
Maria Varabyova <m.warabjowa@gmail.com>
2022-02-06 16:10:47 +0300

    Add OneDimArray3 in maria.varabyova.task3

---
+++ src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray3.java
@@ -0,0 +1,24 @@
+package com.tms.homework.maria.varabyova.task3;
+/*
+3)Создайте массив из 15 случайных целых чисел из отрезка [0; 99]. Выведите
+массив на экран. Подсчитайте сколько в массиве чётных элементов и выведете
+это количество на экран на отдельной строке.
+ */
+
+import java.util.Random;
+
+public class OneDimArray3 {
+    public static void main(String args[]){
+        int count = 0;
+        int a[] = new int[15];
+        for (int key=0; key<a.length; key++) {
+            key= (0+(int)(Math.random() * 99));
+            System.out.print(key+ " ");
+            if (key % 2 ==0) {
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.print("Количество чётных значений: "+count);
+    }
+}
