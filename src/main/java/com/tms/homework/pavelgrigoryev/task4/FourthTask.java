e1152bfabe8eb76bdcca54b23f2c897f428717be
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-06 12:30:02 +0300

    added an additional task#4 in FourthTask

--- src/main/java/com/tms/homework/pavelgrigoryev/task4/FourthTask.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task4/FourthTask.java
@@ -7,10 +7,19 @@ public class FourthTask {
     public static void swim(){
         int[] array = new int[20];
         for (int i = 0; i < array.length; i++){
-            array[i] = (int)(Math.random() * 20);
+            array[i] = ((int)(Math.random() * 20) + 1);
             System.out.print(array[i] + " ");
         }
         System.out.println();
+        int minValue = array[0];
+        for (int j : array) {
+            if (j % 2 != 0) {
+                if (j <= minValue) {
+                    minValue = j;
+                }
+            }
+        }
+        System.out.println("Наименьший нечетный элемент равен " + minValue);
         for (int i = 0; i < array.length; i++){
             if (array[i] % 2 != 0) {
                 array[i] = 0;
