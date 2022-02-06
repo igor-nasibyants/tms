f7fd47bc6cc0c393d018d033979ac322045ecda4
Maria Varabyova <m.warabjowa@gmail.com>
2022-02-06 21:22:37 +0300

    Change OneDimArray05 in maria.varabyova.task3

--- src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray05.java
+++ src/main/java/com/tms/homework/maria/varabyova/task3/OneDimArray05.java
@@ -8,17 +8,32 @@ package com.tms.homework.maria.varabyova.task3;
  */
 public class OneDimArray05 {
     public static void main(String args[]) {
+        double suma1 = 0;
+        double suma2 = 0;
         int a1[] = new int [5];
         int a2[] = new int [5];
 
         for(int i=0; i<a1.length; i++) {
             a1[i]=(0+(int)(Math.random()*15));
             System.out.print(a1[i]+" ");
+            suma1+=(double)a1[i]/a1.length;
         }
+        System.out.println();
 
         for(int j=0; j<a2.length; j++) {
             a2[j]=(0+(int)(Math.random()*15));
             System.out.print(a2[j]+" ");
+            suma2+=(double)a2[j]/a2.length;
+        }
+        System.out.println(" ");
+
+        if(suma1==suma2)
+            System.out.println("Ср.арифм. двух массивов равны: "+suma1+" и "+suma2);
+        else {
+            if (suma1 > suma2)
+                System.out.println("Ср.арифм. первого массива больше: "+suma1+" и "+suma2);
+            else
+                System.out.println("Ср.арифм. второго массива больше: "+suma1+" и "+suma2);
         }
     }
 }
