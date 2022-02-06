496ade89bd1ce7bacacdcb379a81f06fed7fefae
pavelgrigoryev <Green-ono@yandex.ru>
2022-02-06 19:36:51 +0300

    added an additional task#7 in SeventhTask

--- src/main/java/com/tms/homework/pavelgrigoryev/task7/SeventhTask.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task7/SeventhTask.java
@@ -8,6 +8,8 @@ public class SeventhTask {
         int[] array = new int[12];
         int maxValue = 0;
         int lastIndex = 0;
+        int num = 5;
+        int count = 0;
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 15);
             System.out.print(array[i] + " ");
@@ -15,9 +17,13 @@ public class SeventhTask {
                maxValue = array[i];
                lastIndex = i;
            }
+           if (array[i] == num){
+               count++;
+           }
         }
         System.out.println();
         System.out.println(maxValue + " Является макисимальным элементом массива");
         System.out.println(lastIndex + " Index его последнего вхождения в массив");
+        System.out.println(num + " встречается " + count + " раз(а)");
     }
 }
