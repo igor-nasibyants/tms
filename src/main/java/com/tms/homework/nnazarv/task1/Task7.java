c564da8c6bcb0ad57db3f0b01726fb9e5ce763fd
NNazarV <nazarenko-nazar@bk.ru>
2022-02-07 18:42:37 +0300

    Done task 7

---
+++ src/main/java/com/tms/homework/nnazarv/task1/Task7.java
@@ -0,0 +1,24 @@
+package com.tms.homework.nnazarv.task1;
+
+import java.util.Arrays;
+
+public class Task7 {
+    public static void main(String[] args) {
+        int[] array = new int[12];
+        for (int i = 0; i< array.length; i++){
+            array[i] = (int) (Math.random() * 15);
+        }
+        System.out.println(Arrays.toString(array));
+
+        int maxValue = array[0];
+        int maxIndex = 0;
+
+        for (int i = 1; i <= array.length - 1; i++){
+            if (maxValue <= array[i]){
+                maxValue = array[i];
+                maxIndex = i;
+            }
+        }
+        System.out.println("Index " + maxIndex + " : max value " + maxValue);
+    }
+}
