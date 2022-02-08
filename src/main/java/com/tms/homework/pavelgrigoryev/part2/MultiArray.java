edab57b6fdf848d0ac6b3a12c1c54f85e41fa0e0
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-08 15:31:09 +0300

    add package part 2 with MultiArray task0

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/part2/MultiArray.java
@@ -0,0 +1,31 @@
+package com.tms.homework.pavelgrigoryev.part2;
+
+import java.util.Scanner;
+
+public class MultiArray {
+    public static void main(String[] args) {
+        int[][] array;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите первое положительное целое число : ");
+        int firstInput = scanner.nextInt();
+        System.out.println("Введите второе положительное целое число : ");
+        int secondInput = scanner.nextInt();
+
+        while (firstInput < 1 || secondInput < 1) {
+            System.err.println("Ошибка!!! Вы ввели не целое положительное число!");
+            System.out.println("Введите первое положительное целое число : ");
+            firstInput = scanner.nextInt();
+            System.out.println("Введите второе положительное целое число : ");
+            secondInput = scanner.nextInt();
+        }
+
+        array = new int[firstInput][secondInput];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++){
+                array[i][j] =(int) (Math.random() * 50);
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
