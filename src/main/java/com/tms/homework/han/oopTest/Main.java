9b9a53b9f991938b6d8be509adfb8a6a1f9f11e1
Mikita <haniks2899@gmail.com>
2022-02-08 21:06:27 +0300

    Refactoring main

--- src/main/java/com/tms/homework/han/oopTest/Main.java
+++ src/main/java/com/tms/homework/han/oopTest/Main.java
@@ -1,10 +1,6 @@
 package com.tms.homework.han.oopTest;
 
 public class Main {
-    static People people = new People(23, "Nikita", 173);
-    static Man man = new Man(48, "Chel", 185, "asaa", 11111);
-    static Boss boss = new Boss(2, "BOSS", 200, "BOSSES", 99999999, "MONEY");
-    static People[] people1 = {people, man, boss};
 
     public static void massPrint(People[] peoples) {
         for (People person : peoples) {
@@ -13,6 +9,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
+        People people = new People(23, "Nikita", 173);
+        Man man = new Man(48, "Chel", 185, "asaa", 11111);
+        Boss boss = new Boss(2, "BOSS", 200, "BOSSES", 99999999, "MONEY");
+        People[] people1 = {people, man, boss};
+
         people.print();
         man.print();
         boss.print();
