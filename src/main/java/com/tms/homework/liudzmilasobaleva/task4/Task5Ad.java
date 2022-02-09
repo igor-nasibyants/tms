9ce16baeaca763f702970263a1b5efd70d53b95b
Liudzmila Sobaleva <lusya88@gmail.com>
2022-02-09 23:10:31 +0300

    changed Task5Ad to task4

--- src/main/java/com/tms/homework/liudzmilasobaleva/task4/Task5Ad.java
+++ src/main/java/com/tms/homework/liudzmilasobaleva/task4/Task5Ad.java
@@ -2,45 +2,43 @@ package com.tms.homework.liudzmilasobaleva.task4;
 
 public class Task5Ad {
 
-    public class Task5Ad {
-        //5) «Сожмите» массив, выбросив из него каждый второй элемент.
-        //«Освободившиеся» места массива заполните нулями.
-        public static void main(String[] args) {
-            //int[] array = {53, 5, 8, 54, 8, 10, 11};
-            //int[] array = {49, 50, 9, 54, 8, 10, 11};
-            int[] array = {1, 2, 3, 4, 5};
-            //int[] array = {5, 4, 3, 2, 1};
-            //int[] array = {1, 9, 8, 9, 6, 5, 4, 3, 2, 1};
-            //int[] array = {5, 5, 5, 3, 4, 5, 5, 5};
-            //int[] array = {5, 5, 5, 5, 4, 5, 5, 5};
-            //int[] array = {4, 0, 4, 4, 5, 4, 4, 4};
+    //5) «Сожмите» массив, выбросив из него каждый второй элемент.
+    //«Освободившиеся» места массива заполните нулями.
+    public static void main(String[] args) {
+        //int[] array = {53, 5, 8, 54, 8, 10, 11};
+        //int[] array = {49, 50, 9, 54, 8, 10, 11};
+        int[] array = {1, 2, 3, 4, 5};
+        //int[] array = {5, 4, 3, 2, 1};
+        //int[] array = {1, 9, 8, 9, 6, 5, 4, 3, 2, 1};
+        //int[] array = {5, 5, 5, 3, 4, 5, 5, 5};
+        //int[] array = {5, 5, 5, 5, 4, 5, 5, 5};
+        //int[] array = {4, 0, 4, 4, 5, 4, 4, 4};
 
-            //меняем каждый второй элемент на 0
-            for (int i = 0; i < array.length; i++) {
-                if (i % 2 != 0) {
-                    array[i] = 0;
-                } else {
-                    array[i] = array[i];
+        //меняем каждый второй элемент на 0
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 != 0) {
+                array[i] = 0;
+            } else {
+                array[i] = array[i];
+            }
+        }
+
+        //"сжимаем" массив
+        int temp;
+        for (int i = array.length - 1; i >= 0; i--) {
+            if (array[i] == 0) {
+                for (int j = i; j < array.length - 1; j++) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
                 }
             }
+        }
 
-            //"сжимаем" массив
-            int temp;
-            for (int i = array.length - 1; i >= 0; i--) {
-                if (array[i] == 0) {
-                    for (int j = i; j < array.length - 1; j++) {
-                        temp = array[j];
-                        array[j] = array[j + 1];
-                        array[j + 1] = temp;
-                    }
-                }
-            }
-
-            //выводим "сжатый" массив
-            System.out.print("Compressed array: ");
-            for (int i = 0; i < array.length; i++) {
-                System.out.print(array[i] + " ");
-            }
+        //выводим "сжатый" массив
+        System.out.print("Compressed array: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
         }
     }
 }
