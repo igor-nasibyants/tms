abe078a44a0ec5a4217b72f599b08b64522019df
mac <kefir1002@mail.ru>
2022-02-09 22:42:45 +0300

    Add Task1

--- src/main/java/com/tms/homework/staryyo/Task1.java
+++
@@ -1,7 +0,0 @@
-package com.tms.homework.staryyo;
-
-public class Task1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-    }
-}
---
+++ src/main/java/com/tms/homework/staryyo/task1/Task1.java
@@ -0,0 +1,17 @@
+package com.tms.homework.staryyo.task1;
+public class Task1 {
+    public static void main(String[] args) {
+
+        int[] arrays = new int[10];
+        int a, b;
+        for (a = 0, b = 2; a < arrays.length & b <= 20; a++, b += 2) {
+            arrays[a] = b;
+            System.out.print(arrays[a] + " "); // в строку
+        }
+        System.out.println();
+        for (int c : arrays) {
+            System.out.println(c + " ");// в столбик
+        }
+
+    }
+}
