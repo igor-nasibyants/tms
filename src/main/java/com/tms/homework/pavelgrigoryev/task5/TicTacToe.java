3a756d3da5512f915c4135d0a309d032350ccf1d
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-12 22:26:04 +0300

    add TicTacToe in task#5

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task5/TicTacToe.java
@@ -0,0 +1,154 @@
+package com.tms.homework.pavelgrigoryev.task5;
+
+import java.util.Scanner;
+
+public class TicTacToe {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[][] board = {
+                {' ',' ',' '},
+                {' ',' ',' '},
+                {' ',' ',' '}};
+        printBoard(board);
+        while (true) {
+            playerOneTurn(board,scanner);
+            if (isGameFinished(board)) {
+                break;
+            }
+            printBoard(board);
+            playerTwoTurn(board,scanner);
+            if (isGameFinished(board)) {
+                break;
+            }
+            printBoard(board);
+        }
+        scanner.close();
+    }
+    private static boolean isGameFinished(char[][] board) {
+        if (checkForAWinner(board, 'X')) {
+            printBoard(board);
+            System.out.println("Первый игрок победил!!!");
+            return true;
+        }
+        if (checkForAWinner(board, 'O')) {
+            printBoard(board);
+            System.out.println("Второй игрок победил!!!");
+            return true;
+        }
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[i].length; j++){
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        printBoard(board);
+        System.out.println("Ничья!!! :( ");
+        return true;
+    }
+    private static boolean checkForAWinner(char[][] board, char symbol) {
+        if ((board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol)||
+                (board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol)||
+                (board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol)||
+
+                (board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol)||
+                (board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol)||
+                (board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol)||
+
+                (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol)||
+                (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol)){
+            return true;
+        }
+        return false;
+    }
+    private static void playerOneTurn(char[][] board, Scanner scanner) {
+        String userInput;
+        while (true) {
+            System.out.println("Первому игроку приготовиться ввести (1-9) ");
+            userInput = scanner.nextLine();
+            if (isValidMove(board, userInput)) {
+                break;
+            } else {
+                System.err.println(userInput + " неправильный ввод");
+            }
+        }
+        placeMove(board, userInput,'X');
+    }
+    private static void playerTwoTurn(char[][] board, Scanner scanner) {
+        String userInput;
+        while (true) {
+            System.out.println("Второму игроку приготовиться ввести (1-9) ");
+            userInput = scanner.nextLine();
+            if (isValidMove(board, userInput)) {
+                break;
+            } else {
+                System.err.println(userInput + " неправильный ввод");
+            }
+        }
+        placeMove(board, userInput,'O');
+    }
+    private static boolean isValidMove (char[][] board, String position) {
+        switch (position) {
+            case "1":
+                return  (board[0][0] == ' ');
+            case "2":
+                return  (board[0][1] == ' ');
+            case "3":
+                return  (board[0][2] == ' ');
+            case "4":
+                return  (board[1][0] == ' ');
+            case "5":
+                return  (board[1][1] == ' ');
+            case "6":
+                return  (board[1][2] == ' ');
+            case "7":
+                return  (board[2][0] == ' ');
+            case "8":
+                return  (board[2][1] == ' ');
+            case "9":
+                return  (board[2][2] == ' ');
+            default:
+                return false;
+        }
+    }
+    private static void placeMove(char[][] board, String position, char symbol) {
+        switch (position) {
+            case "1":
+                board[0][0] = symbol;
+                break;
+            case "2":
+                board[0][1] = symbol;
+                break;
+            case "3":
+                board[0][2] = symbol;
+                break;
+            case "4":
+                board[1][0] = symbol;
+                break;
+            case "5":
+                board[1][1] = symbol;
+                break;
+            case "6":
+                board[1][2] = symbol;
+                break;
+            case "7":
+                board[2][0] = symbol;
+                break;
+            case "8":
+                board[2][1] = symbol;
+                break;
+            case "9":
+                board[2][2] = symbol;
+                break;
+            default:
+                System.err.println(" Печалька :(");
+        }
+    }
+    private static void printBoard(char[][] board) {
+        System.out.println(board[0][0] + "|" + board[0][1] + "|" + board[0][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[1][0] + "|" + board[1][1] + "|" + board[1][2]);
+        System.out.println("-+-+-");
+        System.out.println(board[2][0] + "|" + board[2][1] + "|" + board[2][2]);
+    }
+}
