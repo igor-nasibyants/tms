13561079539fea4b1d90b7282e90a489697f8103
vlad1581 <vlad.art.91@mail.ru>
2022-02-13 23:41:54 +0300

    add TwoDimeArray in lesson 4 (Tasks 1-2, 3 not end)

---
+++ src/main/java/com/tms/homework/artsiom/vladykouski/task4/TwoDimeArrays/InsertMatrix.java
@@ -0,0 +1,10 @@
+package com.tms.homework.artsiom.vladykouski.task4.TwoDimeArrays;
+
+public class InsertMatrix {
+    public static void main(String[] args) {
+        // Выводим созданную матрицу на консоль.
+        MyArray d = new MyArray();
+        d.getArr();
+
+    }
+}
