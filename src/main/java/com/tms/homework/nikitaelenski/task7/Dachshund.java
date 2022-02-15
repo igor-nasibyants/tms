3de6015f0b0e35fcfb47ac348ac2301586c64015
Никита <nikitaelenski@gmail.com>
2022-02-15 19:02:43 +0300

    Added : correct Task7

---
+++ src/main/java/com/tms/homework/nikitaelenski/task7/Dachshund.java
@@ -0,0 +1,7 @@
+package com.tms.homework.nikitaelenski.task7;
+
+public class Dachshund extends Dog{
+    public void bark(){
+        System.out.println("Гав Гав " + getName());
+    }
+}
---
+++ src/main/java/com/tms/homework/nikitaelenski/task7/GoodSize.java
@@ -0,0 +1,8 @@
+package com.tms.homework.nikitaelenski.task7;
+
+public interface GoodSize {
+    String name = new String();
+    int size = 30;
+
+    public void bark();
+}
