b6584b73e40d009c427abb6e9c4547726f20ebc4
asilius1 <mikita.ogurtsov@gmail.com>
2022-02-17 16:41:56 +0300

    Rename Cars class.

---
+++ src/main/java/com/tms/homework/asilius1/task6/Cars.java
@@ -0,0 +1,28 @@
+package com.tms.homework.asilius1.task6;
+import java.util.Scanner;
+
+
+public class Cars {
+
+    private static Scanner str = new Scanner(System.in);
+
+    public static void info(){
+        Engine diz =  new Engine();
+
+        System.out.println("Ввести тип двигателя ");
+        diz.setType(str.next());
+        System.out.println(System.lineSeparator());
+
+        System.out.println("Ввести объём двигателя " + System.lineSeparator());
+        diz.setPower(readDoubleput());
+        System.out.println(System.lineSeparator());
+
+        System.out.println(diz.toString() + System.lineSeparator());
+
+    }
+
+    public static double readDoubleput() {
+        return str.nextDouble();
+    }
+
+}
