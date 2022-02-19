e07f0ac555b9c1a50250fde3ad6e687b6f60716a
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-02-19 23:04:37 +0300

    fix code

--- src/main/java/com/tms/homework/staryyo/task7/Main.java
+++ src/main/java/com/tms/homework/staryyo/task7/Main.java
@@ -4,9 +4,8 @@ import static com.tms.homework.staryyo.task7.RifleType.AK74;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print(AK47);
+        System.out.println(AK47);
         Rifle ak47 = new Rifle(AK47, 7.62, 1947);
-        System.out.println(AK47.toString());
         ak47.type();
         ak47.defeatedTheEnemy();
 
