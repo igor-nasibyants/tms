e335fb01e07033de0af0ec4dfb68bb354baa242c
asilius1 <mikita.ogurtsov@gmail.com>
2022-02-21 00:03:28 +0300

    Add enum Months.

---
+++ src/main/java/com/tms/homework/asilius1/task7/Data/Months.java
@@ -0,0 +1,16 @@
+package com.tms.homework.asilius1.task7.Data;
+
+public enum Months {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER;
+}
