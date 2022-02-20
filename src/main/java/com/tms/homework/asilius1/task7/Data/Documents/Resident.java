2bbb25caeae396eb9d98276a2576f43b287d03e1
asilius1 <mikita.ogurtsov@gmail.com>
2022-02-21 00:02:17 +0300

    Add enum Resident and Sex.

---
+++ src/main/java/com/tms/homework/asilius1/task7/Data/Documents/Resident.java
@@ -0,0 +1,5 @@
+package com.tms.homework.asilius1.task7.Data.Documents;
+
+public enum Resident {
+    RESIDENT,NO_RESIDENT;
+}
---
+++ src/main/java/com/tms/homework/asilius1/task7/Data/Sex.java
@@ -0,0 +1,5 @@
+package com.tms.homework.asilius1.task7.Data;
+
+public enum Sex {
+    MAN,WOMAN;
+}
