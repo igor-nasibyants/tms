eccd6158b3bd769bca9690d0b369131df7f384f3
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-02-22 19:17:46 +0300

    adjust code

--- src/main/java/com/tms/homework/liudzmilasobaleva/task9/Program.java
+++ src/main/java/com/tms/homework/liudzmilasobaleva/task9/Program.java
@@ -7,14 +7,14 @@ public class Program {
         Pet ksu = new Pet("Ksu", 13);
         System.out.println(ksu.toString());
         ksu.move(); // we'll get "I can run" "I run at home" (overriding)
-        ksu.feedOnMilk(); // we'll get "I feed on milk, I am a Pet"
+        Pet.feedOnMilk(); // we'll get "I feed on milk, I am a Pet"
         ksu.eat();
         ksu.eat("fish");
 
         Mammal m = new Pet ("Asya", 1);
         System.out.println(m.toString());
         m.move(); // we'll get "I can run" "I run at home" (overriding)
-        m.feedOnMilk(); // we'll get "I feed on milk" (hiding methods)
+        Mammal.feedOnMilk(); // we'll get "I feed on milk" (hiding methods)
         m.eat();
         m.eat("carrot");
     }
