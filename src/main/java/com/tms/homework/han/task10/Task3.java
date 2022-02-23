a781d93c9c328c214bdcabe15ab69f2043e76fe1
HaNiks <haniks2899@gmail.com>
2022-02-24 00:44:39 +0300

    pre version Task3

---
+++ src/main/java/com/tms/homework/han/task10/Task3.java
@@ -0,0 +1,18 @@
+package com.tms.homework.han.task10;
+
+import java.util.Arrays;
+
+public class Task3 {
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder(
+                "пупа \n" + "душа \n" + "шалаш \n" + "лупа \n" + "дед \n" + "колобок \n" + "боб \n" +
+                        "зарплата \n".trim());
+//        String str = "пупа \n" + "душа \n" + "шалаш \n" + "лупа \n" + "дед \n" + "колобок \n" + "боб \n" +
+//                "зарплата \n".trim();
+//        String[] str1 = str.split("\n");
+//        System.out.println(Arrays.toString(str1));
+//        for (String word : str1) {
+//
+//        }
+    }
+}
