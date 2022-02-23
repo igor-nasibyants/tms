60077800c255f83c6c55cb32933a1d3208df33ac
mac <kefir1002@mail.ru>
2022-02-23 20:41:10 +0300

    Add interface

--- src/main/java/com/tms/homework/staryyo/task7/Main.java
+++ src/main/java/com/tms/homework/staryyo/task7/Main.java
@@ -5,13 +5,16 @@ import static com.tms.homework.staryyo.task7.RifleType.AK74;
 
 public class Main implements Fire {
     public static void main(String[] args) {
-        System.out.println(AK47);
+        System.out.println();
         Rifle ak47 = new Rifle(AK47, 7.62, 1947);
+        System.out.println(ak47.RifleType);
         ak47.type();
         ak47.defeatedTheEnemy();
 
-        System.out.println(AK74);
+        System.out.println();
         Rifle ak74 = new Rifle(AK74, 5.45, 1974);
+        System.out.println(ak74.RifleType);
+        ak74.RifleType = AK74;
         ak74.type();
         ak74.defeatedTheEnemy();
     }
