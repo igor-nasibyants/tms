29baa1c186ae9325a7fcc776de7493882b3d438d
mac <kefir1002@mail.ru>
2022-02-23 20:29:56 +0300

    Add task7

---
+++ src/main/java/com/tms/homework/staryyo/task7/Fire.java
@@ -0,0 +1,7 @@
+package com.tms.homework.staryyo.task7;
+
+public interface Fire {
+    static void print(){
+        System.out.println("Fire");
+    }
+}
--- src/main/java/com/tms/homework/staryyo/task7/Main.java
+++ src/main/java/com/tms/homework/staryyo/task7/Main.java
@@ -3,7 +3,7 @@ package com.tms.homework.staryyo.task7;
 import static com.tms.homework.staryyo.task7.RifleType.AK47;
 import static com.tms.homework.staryyo.task7.RifleType.AK74;
 
-public class Main {
+public class Main implements Fire {
     public static void main(String[] args) {
         System.out.println(AK47);
         Rifle ak47 = new Rifle(AK47, 7.62, 1947);
