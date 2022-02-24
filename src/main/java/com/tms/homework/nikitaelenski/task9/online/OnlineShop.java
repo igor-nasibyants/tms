1ac9ed9a353cc37aba0bf502b62b82782a3f05fc
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-02-24 21:27:25 +0300

    fix examples

--- src/main/java/com/tms/homework/nikitaelenski/task9/online/OnlineShop.java
+++ src/main/java/com/tms/homework/nikitaelenski/task9/online/OnlineShop.java
@@ -5,49 +5,49 @@ import com.tms.homework.nikitaelenski.task9.Shop;
 
 public class OnlineShop extends Shop implements Opening {
 
-private final Integer numberofOperatorsInTheCallCenter;
+    private final Integer numberofOperatorsInTheCallCenter;
 
 
-    public OnlineShop(String adress, Integer quantityGoods, Integer pruduktVariable, String salesMArket ,
+    public OnlineShop(String adress, Integer quantityGoods, Integer productVariable, String salesMarket,
                       Integer numberofOperatorsInTheCallCenter) {
-        super(adress, quantityGoods, pruduktVariable, salesMArket);
+        super(adress, quantityGoods, productVariable, salesMarket);
         this.numberofOperatorsInTheCallCenter = numberofOperatorsInTheCallCenter;
     }
 
-    public void addressСorrection(){
-        if (getSalesMArket().equalsIgnoreCase("Беларусь")){
-            if(getAdress().substring(getAdress().length()-3 ,getAdress().length()-0).equals("com")) {
-                int adress = getAdress().length();
-                String newAdress = getAdress().substring(0, adress - 3) + "by";
+    public void addressСorrection() {
+        if (getSalesMArket().equalsIgnoreCase("Беларусь")) {
+            if (getAdress().endsWith("com")) {
+                String newAdress = getAdress().replace(".com", ".by");
                 setAdress(newAdress);
                 System.out.println(newAdress);
             }
-        }else{
+        } else {
             System.out.println(getAdress());
         }
     }
 
-    public void printbl(String B){
-        if (B.equals("Беларусь")){
+    public void printbl(String B) {
+        if (B.equals("Беларусь")) {
             System.out.println("Отлично");
-        }else {
+        } else {
             System.out.println("Плохо");
         }
 
     }
-    public void  seatCalculationForOperators(){
-        if(numberofOperatorsInTheCallCenter >5 ){
+
+    public void seatCalculationForOperators() {
+        if (numberofOperatorsInTheCallCenter > 5) {
             System.out.println("Нужен небольшой офис.");
-        }else if (numberofOperatorsInTheCallCenter >10 ){
+        } else if (numberofOperatorsInTheCallCenter > 10) {
             System.out.println("Нужен офис побольше ");
-        }else if (numberofOperatorsInTheCallCenter >20 ){
+        } else if (numberofOperatorsInTheCallCenter > 20) {
             System.out.println("Нужен офис достаточной вместительностью .");
-        }else if (numberofOperatorsInTheCallCenter >50 ){
+        } else if (numberofOperatorsInTheCallCenter > 50) {
             System.out.println("Нужен ужасно большой офис .");
-        }else{
+        } else {
             System.out.println("Нужен гиганский офис.");
         }
-        System.out.println("инвентарь в офисе:\nСтолы : " +  numberofOperatorsInTheCallCenter +
+        System.out.println("инвентарь в офисе:\nСтолы : " + numberofOperatorsInTheCallCenter +
                 "\nСтулья " + numberofOperatorsInTheCallCenter
         );
     }
@@ -57,9 +57,11 @@ private final Integer numberofOperatorsInTheCallCenter;
         Integer timeJob = 24;
         return "Время работы нашего онлайн магазина " + timeJob + " часа";
     }
-    public String openShop(){
+
+    public String openShop() {
         return "Мы открыт ";
     }
+
     public String closeShop() {
         return "Мы закрыт ";
     }
