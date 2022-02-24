aabaf8a84d6c80bb5e3480a9db38f636cfa86c95
Grigoryev Pavel <Green-ono@yandex.ru>
2022-02-24 15:02:29 +0300

    add Task1 and Task2 to package task10

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task10/Task1.java
@@ -0,0 +1,26 @@
+package com.tms.homework.pavelgrigoryev.task10;
+
+public class Task1 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder("""
+                От печали я очнулся и невольно усмехнулся,
+                Видя важность этой птицы, жившей долгие года.
+                «Твой хохол ощипан славно и глядишь ты презабавно»,
+                Я промолвил, «но скажи мне: в царстве тьмы, где ночь всегда,
+                Как ты звался, гордый Ворон, там, где ночь царит всегда!»
+                Молвил Ворон: «Никогда».""");
+        print(stringBuilder);
+    }
+    private static void print(StringBuilder stringBuilder) {
+        separator();
+        System.out.println(stringBuilder);
+        separator();
+        System.out.println("Вырезанный текст от символа \"я\" до символа \"ж\" : ");
+        separator();
+        System.out.println(stringBuilder.substring(stringBuilder.indexOf("я"), stringBuilder.indexOf("ж")));
+        separator();
+    }
+    public static void separator() {
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+    }
+}
---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task10/Task2.java
@@ -0,0 +1,25 @@
+package com.tms.homework.pavelgrigoryev.task10;
+
+public class Task2 extends Task1 {
+    public static void main(String[] args) {
+        String string = """
+                I am a world before I am a man
+                I was a creature before I could stand
+                I will remember before I forget
+                Before I forget that
+                I am a world before I am a man
+                I was a creature before I could stand
+                I will remember before I forget
+                Before I forget that""";
+        print(string);
+    }
+    private static void print(String string) {
+        separator();
+        System.out.println(string);
+        separator();
+        System.out.println("Заменены все вхождения символа стоящего в позиции(3) на символ стоящий в позиции(0) : ");
+        separator();
+        System.out.println(string.replace(String.valueOf(string.charAt(3)),String.valueOf(string.charAt(0))));
+        separator();
+    }
+}
