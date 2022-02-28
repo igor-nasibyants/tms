8f4d3b47da51478f26e944a632a36baf9ed8cb53
Mikita <haniks2899@gmail.com>
2022-02-28 16:45:01 +0300

    Task2 is refactoring

--- src/main/java/com/tms/homework/han/task11/task2/ColorException.java
+++ src/main/java/com/tms/homework/han/task11/task2/ColorException.java
@@ -3,6 +3,6 @@ package com.tms.homework.han.task11.task2;
 public class ColorException extends Exception {
 
     public ColorException(String s) {
-        System.err.println(s);
+        super(s);
     }
 }
