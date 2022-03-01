63c999307dc74957257550e8bc6f6886da57bddc
mac <kefir1002@mail.ru>
2022-03-01 11:33:18 +0300

    Add substring

---
+++ src/main/java/com/tms/homework/staryyo/Task9/Main.java
@@ -0,0 +1,10 @@
+package com.tms.homework.staryyo.Task9;
+// вырезаем подстроку
+
+public class Main {
+    public static void main(String[] args) {
+        String str1 = new String("Patatoes");
+        String string = str1.substring(2, 6);
+        System.out.println(string);
+    }
+}
