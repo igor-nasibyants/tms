11f4b3ba82f3e426414c4462100da466939ae0f2
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-03-01 19:45:24 +0300

    fix examples

--- src/main/java/com/tms/homework/pavelgrigoryev/task11/secondtask/Main.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task11/secondtask/Main.java
@@ -3,41 +3,22 @@ package com.tms.homework.pavelgrigoryev.task11.secondtask;
 public class Main {
     public static void main(String[] args) {
         Plants[] plants = new Plants[5];
+        String[] colors = new String[]{"GREEN", "YELLOW", "Red", "viOLet", "Яловый"};
+        String[] types = new String[]{"mosses", "SEAWEED", "Сосна", "FERNs", "Цвяточык"};
+        int[] sizes = new int[]{20, 30, 24, 55, 11};
 
-        try {
-            plants[0] = new Plants(20,Color.color("GREEN"),Type.type("mosses"));
-        } catch (ColorException | TypeException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(plants[0]);
+        for (int i = 0; i < 5; i ++) {
+            try {
+                plants[i] = buildPlants(sizes[i], colors[i], types[i]);
+            } catch (ColorException | TypeException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(plants[i]);
+            }
         }
-        try {
-            plants[1] = new Plants(30,Color.color("YELLOW"),Type.type("SEAWEED"));
-        } catch (ColorException | TypeException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(plants[1]);
-        }
-        try {
-            plants[2] = new Plants(24,Color.color("Red"),Type.type("Сосна"));
-        } catch (ColorException | TypeException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(plants[2]);
-        }
-        try {
-            plants[3] = new Plants(55,Color.color("viOLet"),Type.type("FERNs"));
-        } catch (ColorException | TypeException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(plants[3]);
-        }
-        try {
-            plants[4] = new Plants(11,Color.color("Яловый"),Type.type("Цвяточык"));
-        } catch (ColorException | TypeException e) {
-            e.printStackTrace();
-        } finally {
-            System.out.println(plants[4]);
-        }
+    }
+
+    private static Plants buildPlants(int size, String color, String type) throws ColorException, TypeException {
+        return new Plants(size, Color.color(color), Type.type(type));
     }
 }
