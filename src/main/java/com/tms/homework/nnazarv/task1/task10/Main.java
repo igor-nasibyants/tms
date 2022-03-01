ffacd38cc2bfd74946596a2d97972b25f1a15c96
NNazarV <nazarenko-nazar@bk.ru>
2022-03-01 17:17:11 +0300

    test string methods

---
+++ src/main/java/com/tms/homework/nnazarv/task1/task10/Main.java
@@ -0,0 +1,20 @@
+package com.tms.homework.nnazarv.task1.task10;
+
+public class Main {
+    public static void main(String[] args) {
+        String str = "Hello";
+        char[] array = str.toCharArray();
+
+        for (int i = 0; i < array.length; i++){
+            System.out.println(array[i]);
+
+
+        }
+
+        System.out.println("----------");
+        String str2 = str.substring(0, 3);
+        System.out.println(str2);
+
+
+    }
+}
