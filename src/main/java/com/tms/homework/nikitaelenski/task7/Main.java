c0a48a3d2da2bf0e44999c365c29d81441531450
Никита <nikita1998-15@mail.ru>
2022-03-02 20:29:16 +0300

    added homework with OutputStream

--- src/main/java/com/tms/homework/nikitaelenski/task7/Main.java
+++ src/main/java/com/tms/homework/nikitaelenski/task7/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static void main(String[] args) {
         String smooth = "=";
         SausageDoge BassetHound = new SausageDoge(70, "Сало", "Джордж", false);
-        System.out.println(smooth.repeat(10));
+//        System.out.println(smooth.repeat(10));
         SausageDoge Corgi = new SausageDoge(20, "Мясо", "Джордж", true);
         BassetHound.TouchTheDog();
         BassetHound.eatTheDog();
-        System.out.println(smooth.repeat(10));
+//        System.out.println(smooth.repeat(10));
         Corgi.eatTheDog();
         Corgi.TouchTheDog();
-        System.out.println(smooth.repeat(10));
+//        System.out.println(smooth.repeat(10));
     }
 }
 */
