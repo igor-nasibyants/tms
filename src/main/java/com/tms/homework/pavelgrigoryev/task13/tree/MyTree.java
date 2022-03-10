c80a8da44a8465dcfc103ac446290601b926235f
Grigoryev Pavel <Green-ono@yandex.ru>
2022-03-10 13:04:44 +0300

    add in package task#13 package tree with class MyTree

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task13/tree/MyTree.java
@@ -0,0 +1,26 @@
+package com.tms.homework.pavelgrigoryev.task13.tree;
+
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class MyTree {
+    public static void main(String[] args) {
+        SortedSet<String> set = new TreeSet<>();
+        set.add("Poland");
+        set.add("Belarus");
+        set.add("Germany");
+        set.add("Japan");
+        set.add("Angola");
+        set.add("Zanzibar");
+
+        System.out.println(set);
+
+        SortedSet<String> subSet = set.subSet("Belarus","Poland");
+        System.out.println("Subset: " + subSet);
+
+        System.out.println("HeadSet: " + set.headSet("Japan"));
+        System.out.println("TailSet: " + set.tailSet("Japan"));
+        System.out.println("FirstElement: " + set.first());
+        System.out.println("LastElement: " + set.last());
+    }
+}
