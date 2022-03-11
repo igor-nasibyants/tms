de4c0c979b037b2c7f6a878f3b48cdeabf324bdb
Mikita <haniks2899@gmail.com>
2022-03-11 15:30:35 +0300

    add LocalDate.now

---
+++ src/main/java/com/tms/homework/han/task14/LocalDateExample.java
@@ -0,0 +1,10 @@
+package com.tms.homework.han.task14;
+
+import java.time.LocalDate;
+
+public class LocalDateExample {
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.now();
+
+    }
+}
