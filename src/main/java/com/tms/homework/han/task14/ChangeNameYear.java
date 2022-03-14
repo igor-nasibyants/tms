ebdb5ff27000b3d359cb6d4bbade77adb2b18b17
HaNiks <haniks2899@gmail.com>
2022-03-14 21:23:19 +0300

    Refactoring code

--- src/main/java/com/tms/homework/han/task14/ChangeNameYear.java
+++ src/main/java/com/tms/homework/han/task14/ChangeNameYear.java
@@ -2,15 +2,14 @@ package com.tms.homework.han.task14;
 
 public interface ChangeNameYear {
 
-    static void changeNameYear(Integer age, String name, Integer minAge) {
-        if (age >= minAge) {
-            if (age % 10 == 1 && age != 11) {
-                System.out.println("Cat " + name + ", " + age + " years old");
-            } else if (age % 10 > 1 && age < 5) {
-                System.out.println("Cat " + name + ", " + age + " years old");
-            } else {
-                System.out.println("Cat " + name + ", " + age + " years old");
-            }
+    static void changeNameYear(Integer age, String name) {
+
+        if (age % 10 == 1 && age != 11) {
+            System.out.println("Котик " + name + ", " + age + " год / Cat " + name + ", " + age + " years old");
+        } else if (age % 10 > 1 && age < 5) {
+            System.out.println("Котик " + name + ", " + age + " года / Cat " + name + ", " + age + " years old");
+        } else {
+            System.out.println("Котик " + name + ", " + age + " лет / Cat " + name + ", " + age + " years old");
         }
     }
 }
