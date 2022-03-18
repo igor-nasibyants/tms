6669d59f6d57a594a9868d797d32de8f919875ca
Grigoryev Pavel <Green-ono@yandex.ru>
2022-03-18 22:10:56 +0300

    modified task#16

--- src/main/java/com/tms/homework/pavelgrigoryev/task16/Streams.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task16/Streams.java
@@ -1,14 +1,18 @@
 package com.tms.homework.pavelgrigoryev.task16;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Streams {
     public static void main(String[] args) {
-
+        Streams streams = new Streams();
+        streams.terminate();
     }
 
-    private List<Dwarf> dwarves = List.of(
+    private final List<Dwarf> dwarves = List.of(
             new Dwarf("Muradin","Bronzebeared",121,56,Role.TANK),
             new Dwarf("Moyra","Taurissan",233,40,Role.HEALER),
             new Dwarf("Falstad","Thunderforge",1212,50,Role.MELEE_DAMAGE_DEALER),
@@ -19,20 +23,40 @@ public class Streams {
             new Dwarf("Ferya","Chunderstout",600,33,Role.RANGED_DAMAGE_DEALER)
     );
 
+    public void terminate() {
+        sep();
+        Stream<Dwarf> stream = dwarves.stream();
+        System.out.println(stream.count());
+        sep();
 
-
-    private void print(Stream<Dwarf> stream) {
-        stream
-                .map(emp -> String.format(
-                        "%4d | %-15s %-10s age %s %s",
-                        emp.getId(),
-                        emp.getLastName(),
-                        emp.getFirstName(),
-                        emp.getAge(),
-                        emp.getRole()
-                ))
-                .forEach(System.out::println);
-
+        dwarves.forEach(dwarf -> System.out.print(dwarf.getAge() + " "));
         System.out.println();
+        sep();
+
+        Map<Integer, String> collect = dwarves.stream().collect(Collectors.toMap(
+                Dwarf::getId,
+                dwarf -> String.format("%s %s", dwarf.getLastName(), dwarf.getFirstName())
+        ));
+        System.out.println(collect);
+        sep();
+
+        System.out.println(dwarves.stream().max(Comparator.comparingInt(Dwarf::getAge)));
+        sep();
+
+        System.out.println(dwarves.stream().findAny());
+        sep();
+        System.out.println(dwarves.stream().findFirst());
+        sep();
+
+        System.out.println(dwarves.stream().noneMatch(dwarf -> dwarf.getAge() > 60));
+        sep();
+        System.out.println(dwarves.stream().anyMatch(dwarf -> dwarf.getRole() == Role.TANK));
+        sep();
+        System.out.println(dwarves.stream().allMatch(dwarf -> dwarf.getAge() > 18));
+        sep();
+    }
+
+    public void sep() {
+        System.out.println("==========================================================================================");
     }
 }
