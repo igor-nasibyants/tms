50e65f2cc006afdf36cceee5cf1f1f6a230c5fab
mac <kefir1002@mail.ru>
2022-03-22 13:16:39 +0300

    Add Hero

--- src/main/java/com/tms/homework/staryyo/Task15/Heroes.java
+++ src/main/java/com/tms/homework/staryyo/Task15/Heroes.java
@@ -1,35 +1,23 @@
 package com.tms.homework.staryyo.Task15;
-
 import java.util.Objects;
+import java.util.Optional;
 
 
 public class Heroes {
-    private final String name;
-    private final Integer power;
-    private final String complexity;
+   private final String name;
+   private final Integer power;
+   private final String comlexity;
 
-    public Heroes(String name, Integer power, String complexity) {
-        this.name = name;
-        this.power = power;
-        this.complexity = complexity;
-    }
-
-    public String getComplexity() {
-        return name;
-    }
-
-    public String getName() {
-        return complexity;
-    }
-
-    public Integer getPower() {
-        return power;
-    }
-
-    @Override
-    public String toString() {
-        return name + "-" + power + "-" + complexity;
-    }
+   public Heroes(String name, Integer power, String comlexity){
+       this.name = name;
+       this.power = power;
+       this.comlexity = comlexity;
+   }
+   public String getComlexity(){return name ;}
+   public String getName(){return comlexity;}
+   public Integer getPower(){return power;}
+   @Override
+    public String toString(){return name + "-" +  power + "-" + comlexity;}
 
 
     @Override
@@ -39,7 +27,7 @@ public class Heroes {
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, power, complexity);
+        return Objects.hash(name, power, comlexity);
     }
 }
 
--- src/main/java/com/tms/homework/staryyo/Task15/Main.java
+++ src/main/java/com/tms/homework/staryyo/Task15/Main.java
@@ -1,14 +1,20 @@
 package com.tms.homework.staryyo.Task15;
 
+
+import java.util.ArrayList;
+import java. util.List;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Heroes Odet = new Heroes("Odett", 6, "Easy");
-        Heroes Zask = new Heroes("Zask", 5, "Medium");
-        Heroes Aldous = new Heroes("Aldous", 9, "Hard");
-        Heroes Lancelot = new Heroes("Lancelot", 10, "Hard");
+        Heroes Odet = new Heroes("Odett", "6", "Easy");
+        Heroes Zask = new Heroes("Zask", "5", "Medium");
+        Heroes Aldous = new Heroes("Aldous", "9", "Hard");
+        Heroes Lancelot = new Heroes("Lancelot", "10", "Hard");
+
+
+
 
 
     }
@@ -16,12 +22,12 @@ public class Main {
     public static void getHeroes(String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isRepeat = true;
-        while (isRepeat) {
+        while (isRepeat){
             System.out.println("Enter name Hero/ введите имя героя");
             String nameHero = scanner.nextLine();
             System.out.println("Hero chose");
             break;
         }
     }
-}
+    }
 
