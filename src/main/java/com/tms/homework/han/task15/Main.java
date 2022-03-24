5b15e1fec03531ab5dc8b858b40a2a8950e1a96c
Mikita <haniks2899@gmail.com>
2022-03-24 10:43:22 +0300

    add List

--- src/main/java/com/tms/homework/han/task15/Main.java
+++ src/main/java/com/tms/homework/han/task15/Main.java
@@ -2,7 +2,9 @@ package com.tms.homework.han.task15;
 
 import com.tms.homework.han.task14.Cat;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
@@ -10,6 +12,21 @@ import static com.tms.homework.han.task14.Main.getSetCats;
 
 public class Main {
     public static void main(String[] args) {
+        //Через List
+        Cat a = new Cat(15, 10, "a");
+        Cat b = new Cat(5, 18, "b");
+        Cat l = new Cat(1, 5, "l");
+        List<Cat> c = new ArrayList<>();
+        c.add(a);
+        c.add(b);
+        c.add(l);
+        Map<Integer, String> catsForNik = new HashMap<>();
+        catsForNik.putAll(c.stream()
+                .collect(Collectors.toMap(Cat::getId, Cat::getName, (oldValue, newValue) -> newValue)));
+        catsForNik.forEach((k, v) -> System.out.println("Котику " + v + ", " + k + " " + changeEndOfAgeWord(k)));
+        System.out.println("========================================");
+        //////////////////////////////
+
         Map<Integer, String> catsMap = new HashMap<>(30, 1);
         catsMap.putAll(getSetCats().stream()
                 .filter(cat -> cat.getAge() >= 0 && cat.getAge() < 20)
