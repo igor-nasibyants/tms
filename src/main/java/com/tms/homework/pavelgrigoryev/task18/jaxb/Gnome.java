bd497844ac7c6327707977836ec6a497c20823a8
Grigoryev Pavel <Green-ono@yandex.ru>
2022-03-30 13:09:32 +0300

    add in task#18 JAXB

---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task18/jaxb/Gnome.java
@@ -0,0 +1,48 @@
+package com.tms.homework.pavelgrigoryev.task18.jaxb;
+
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Gnome {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    @XmlAttribute
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @XmlElement
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @XmlElement
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Gnome{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
---
+++ src/main/java/com/tms/homework/pavelgrigoryev/task18/jaxb/JavaArchitectureForXMLBinding.java
@@ -0,0 +1,44 @@
+package com.tms.homework.pavelgrigoryev.task18.jaxb;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
+import jakarta.xml.bind.Unmarshaller;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
+
+public class JavaArchitectureForXMLBinding {
+    public static void main(String[] args) throws JAXBException, FileNotFoundException {
+        jaxbWriter();
+        System.out.println("=====================================================================");
+        jaxbReader();
+    }
+
+    private static void jaxbWriter() throws JAXBException, FileNotFoundException {
+        Gnome gnome = new Gnome();
+        gnome.setId(1);
+        gnome.setName("MilkHouse");
+        gnome.setAge(45);
+
+        OutputStream outputStream = new FileOutputStream("xml//gnome.xml");
+        JAXBContext context = JAXBContext.newInstance(Gnome.class);
+        Marshaller marshaller = context.createMarshaller();
+
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+
+        marshaller.marshal(gnome, outputStream);
+        marshaller.marshal(gnome, System.out);
+    }
+
+    private static void jaxbReader() throws JAXBException {
+        File file = new File("xml//gnome.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(Gnome.class);
+
+        Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
+        Gnome gnome = (Gnome) unmarshaller.unmarshal(file);
+        System.out.println(gnome);
+    }
+}
