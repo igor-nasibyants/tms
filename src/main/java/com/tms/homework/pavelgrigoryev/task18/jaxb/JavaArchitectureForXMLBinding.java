6bcd06ed427adee37964e931711bdac2682fc8d8
Grigoryev Pavel <Green-ono@yandex.ru>
2022-03-30 13:24:45 +0300

    add fix in task#18 JAXB

--- src/main/java/com/tms/homework/pavelgrigoryev/task18/jaxb/JavaArchitectureForXMLBinding.java
+++ src/main/java/com/tms/homework/pavelgrigoryev/task18/jaxb/JavaArchitectureForXMLBinding.java
@@ -5,10 +5,7 @@ import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
+import java.io.*;
 
 public class JavaArchitectureForXMLBinding {
     public static void main(String[] args) throws JAXBException, FileNotFoundException {
@@ -33,12 +30,12 @@ public class JavaArchitectureForXMLBinding {
         marshaller.marshal(gnome, System.out);
     }
 
-    private static void jaxbReader() throws JAXBException {
-        File file = new File("xml//gnome.xml");
+    private static void jaxbReader() throws JAXBException, FileNotFoundException {
+        FileInputStream fileInputStream = new FileInputStream("xml//gnome.xml");
         JAXBContext jaxbContext = JAXBContext.newInstance(Gnome.class);
 
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Gnome gnome = (Gnome) unmarshaller.unmarshal(file);
+        Gnome gnome = (Gnome) unmarshaller.unmarshal(fileInputStream);
         System.out.println(gnome);
     }
 }
