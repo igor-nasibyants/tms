b302b7735aeea52e554065ef5a0f21af9b9e85d5
Никита <nikitaelenski@gmail.com>
2022-04-10 23:27:21 +0300

    Aries task 19

--- src/main/java/com/tms/homework/nikitaelenski/task19/exercise1/Main.java
+++ src/main/java/com/tms/homework/nikitaelenski/task19/exercise1/Main.java
@@ -1,11 +1,15 @@
 package com.tms.homework.nikitaelenski.task19.exercise1;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     static Streams t1 = new Streams(1);
     static Streams t2 = new Streams(2);
     static Streams t3 = new Streams(3);
 
     public static void main(String[] args) throws InterruptedException {
+        Lock lock = new ReentrantLock();
         t3.start();
         t3.join();
         t2.start();
