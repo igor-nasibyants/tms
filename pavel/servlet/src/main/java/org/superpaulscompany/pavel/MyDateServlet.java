cce1e5f3e6c19642af9a48ed8f064f56609946b4
Grigoryev Pavel <Green-ono@yandex.ru>
2022-04-17 22:06:38 +0300

    added a LocalDateTime

--- pavel/servlet/src/main/java/org/superpaulscompany/pavel/MyDateServlet.java
+++ pavel/servlet/src/main/java/org/superpaulscompany/pavel/MyDateServlet.java
@@ -5,11 +5,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.LocalDateTime;
 
 @WebServlet("/servlet")
 public class MyDateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.getWriter().print("Fool, you're lost in time.");
+        resp.getWriter().println("Fool, you're lost in time.\n");
+        LocalDateTime localDateTime = LocalDateTime.of(1999,12,31,23,59,59);
+        resp.getWriter().println(localDateTime);
     }
 }
