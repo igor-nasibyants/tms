b3a3c222c7a3f0267e686327c87d916e6cc42118
Grigoryev Pavel <Green-ono@yandex.ru>
2022-04-18 15:52:10 +0300

    add HttpSession example

--- pavel/servlet/src/main/java/org/superpaulscompany/pavel/MyDateServlet.java
+++ pavel/servlet/src/main/java/org/superpaulscompany/pavel/MyDateServlet.java
@@ -3,17 +3,30 @@ package org.superpaulscompany.pavel;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.concurrent.atomic.AtomicReference;
 
 
 public class MyDateServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         resp.setContentType("text/html");
+
+        HttpSession session = req.getSession();
+
+        Integer count = (Integer) session.getAttribute("count");
+
+        if (count == null) {
+            session.setAttribute("count",1);
+            count = 1;
+        } else {
+            session.setAttribute("count", ++count);
+        }
 
         PrintWriter printWriter = resp.getWriter();
         printWriter.println("Fool, you're lost in time.\n");
@@ -26,9 +39,11 @@ public class MyDateServlet extends HttpServlet {
         ArrayList<LocalDateTime> localDateTimeArrayList = new ArrayList<>();
         Collections.addAll(localDateTimeArrayList, localDateTime, localDateTime1, localDateTime2, localDateTime3);
 
+        AtomicReference<Integer> finalCount = new AtomicReference<>(count);
         localDateTimeArrayList
                 .stream()
                 .sorted()
-                .forEach(x -> printWriter.println("<html>" + "<h1>" + x + "</h1>" + "</html>"));
+                .forEach(x -> printWriter.println("<html>" + "<h1>" + "Number changes from session : "
+                        + (finalCount.getAndSet(finalCount.get() + 1)) + " | Date: " + x + "</h1>" + "</html>"));
     }
 }
