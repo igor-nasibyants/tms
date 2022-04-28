8d3b131b7a35d2eb492eb77b8888fc47b821cf68
Никита <nikita1998-15@mail.ru>
2022-04-28 15:02:25 +0300

    mb fixed

--- servlet/src/main/java/org/mycompany/myname/HelloServlet.java
+++ servlet/src/main/java/org/mycompany/myname/HelloServlet.java
@@ -1,20 +1,13 @@
 package org.mycompany.myname;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 @WebServlet("/servlet")
 public class HelloServlet extends HttpServlet {
-
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-
-        HttpSession session = request.getSession();
-        session.setAttribute("name", "Tom");
-        session.setAttribute("age", 34);
-
-        getServletContext().getRequestDispatcher("/basic.jsp").forward(request, response);
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
     }
 }
