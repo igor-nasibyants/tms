eac32db87c4edcdd1be1266eda214888569ce3bf
igor-nasibyants <igor.nasibyants@moneyman.ru>
2022-05-17 17:03:58 +0300

    add builder example

---
+++ c54/src/main/java/com/tms/task/task30/creational/builder/Company.java
@@ -0,0 +1,5 @@
+package com.tms.task.task30.creational.builder;
+
+public abstract class Company extends CD {
+    public abstract int price();
+}//End of the Company class.
---
+++ c54/src/main/java/com/tms/task/task30/creational/builder/Packing.java
@@ -0,0 +1,7 @@
+package com.tms.task.task30.creational.builder;
+
+public interface Packing {
+    public String pack();
+
+    public int price();
+}// End of Packing interface.
