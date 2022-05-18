24b7345875322d5d2cd5d3dcfd4d9cb2ccfd41cc
НикитаE <nikitaelenski@gmailcom>
2022-05-18 22:52:03 +0300

    Add slight changes

---
+++ c54/src/main/java/com/tms/homework/nikitaelenski/task30/patterns/builder/Main.java
@@ -0,0 +1,19 @@
+package com.tms.homework.nikitaelenski.task30.patterns.builder;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Director director = new Director();
+        int random = (int) (1+Math.random()*2);
+        if(random ==1) {
+            director.setBuilder(new VisitCartWebsiteBuilder());
+            Website website = director.buildWebsite();
+            System.out.println(website);
+        } else if (random ==2) {
+            director.setBuilder(new EnterpriceWebsiteBuilder());
+            Website website = director.buildWebsite();
+            System.out.println(website);
+        }
+    }
+}
