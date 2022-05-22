ebfd7d5a440b527170f957320f397ff43a17a1e8
Mikita <haniks2899@gmail.com>
2022-05-22 14:03:04 +0300

    add singleton with DB

---
+++ c54/src/main/java/com/tms/homework/han/creational/singleton/Task.java
@@ -0,0 +1,40 @@
+package com.tms.homework.han.creational.singleton;
+
+public class Task {
+    int id;
+    String name;
+    boolean status;
+
+    public Task() {
+    }
+
+    public Task(int id, String name, boolean status) {
+        this.id = id;
+        this.name = name;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+}
